55722a53fd7528561cb1034e71f4543907390670
zhangmaorui <1608915508@qq.com>
2020-02-28 13:22:15 +0800

    web项目

---
+++ src/com/alibaba/bean.java
@@ -0,0 +1,12 @@
+package com.alibaba;
+
+/**
+ * @auhor Marvin
+ * @create 2020--02--28--13:13
+ */
+public class bean {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+
+}
